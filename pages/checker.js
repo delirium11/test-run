73241ade06f7a8a10a541c23a88525c1cd2bb91c
delirium11 <pervane.uk@gmail.com>
2023-04-16 18:12:26 -0400

    commit 10

--- pages/checker.js
+++ pages/checker.js
@@ -3,7 +3,6 @@ function Checker() {
     return (
         <div className="testing">
             <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-            <link rel="stylesheet" href="index.css"/>  
             <h1 className="checker-warning">THIS IS A TEST SENTENCE</h1>
             <a>Home</a>
             <div className="checker-description">
