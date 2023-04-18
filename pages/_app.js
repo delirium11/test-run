e0ebc86bf55ef9bc5af5a05ca2e60973ab238b40
delirium11 <pervane.uk@gmail.com>
2023-04-18 12:53:58 -0400

    commit 22

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,14 @@ import Navbar from '@/components/navbar'
 export default function App ({ Component, pageProps }) {
 
     return (
+
         <div>
 
             <Navbar />
             <Component {...pageProps} />
 
         </div>
+        
     )
 
 }
