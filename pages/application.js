e1985a9c4a79ebea26235358dcdb61c234fda49e
delirium11 <pervane.uk@gmail.com>
2023-04-18 22:35:42 -0400

    commit 26

---
+++ pages/application.js
@@ -0,0 +1,16 @@
+
+
+export default function Application() {
+
+    return (
+
+        <div className="text_content">
+
+            <h1>APPLICATION PAGE</h1>
+            <p>THIS IS THE APPLICATION PAGE</p>
+        
+        </div> 
+
+    )
+    
+}
