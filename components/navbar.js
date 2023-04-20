c1d3b091edcec5815b16d2c6016b570e47e5ea0d
delirium11 <pervane.uk@gmail.com>
2023-04-20 01:00:31 -0400

    commit 36

--- components/navbar.js
+++ components/navbar.js
@@ -4,12 +4,12 @@ import { faSailboat } from "@fortawesome/free-solid-svg-icons";
 import { faTwitter, faMedium, faDiscord } from "@fortawesome/free-brands-svg-icons";
 
 export default function Navbar() {
-
+    
     return (
 
         <div>
 
-            <div className="logo_mobile_container">
+            <div>
 
                 <Link href="/"><img className='logo_mobile' 
                     src="/images/logo_long.png" alt="logo_long"/></Link>
@@ -17,7 +17,7 @@ export default function Navbar() {
             </div>
 
             <div className="navbar">
-        
+
                 <div>
 
                     <Link href="/"><img className="logo_desktop" 
@@ -25,23 +25,23 @@ export default function Navbar() {
 
                 </div>
 
-                <div>
+                <div className="navbar_content">
 
-                    <li><button><Link href="/checker">CHECKER</Link></button></li>
-                    
-                    <li><button><Link href="/mint">MINT</Link></button></li>
+                    <button><Link href="/checker">CHECKER</Link></button>
 
-                    <button><a 
-                        target="_blank" rel="noreferrer">
-                            <FontAwesomeIcon icon={faTwitter}/></a></button>
+                    <button><Link href="/mint">MINT</Link></button>
 
                     <button><a 
                         target="_blank" rel="noreferrer">
-                            <FontAwesomeIcon icon={faMedium}/></a></button>
+                        <FontAwesomeIcon icon={faTwitter}/></a></button>
 
                     <button><a 
                         target="_blank" rel="noreferrer">
-                            <FontAwesomeIcon icon={faSailboat}/></a></button>
+                        <FontAwesomeIcon icon={faMedium}/></a></button>
+
+                    <button><a 
+                        target="_blank" rel="noreferrer">
+                        <FontAwesomeIcon icon={faSailboat}/></a></button>
 
                     <button><a>CONNECT</a></button>
 
--- pages/checker.js
+++ pages/checker.js
@@ -1,6 +1,31 @@
-
+import React from "react";
+import { create } from "ipfs-http-client";
+import { useState, useEffect } from "react";
 
 export default function Checker() {
+
+    const [ newList, setList ] = useState([]);
+    const [ wallet, setWallet] = useState ("");
+    const [ status, setStatus] = useState("");
+
+    const ipfs = create('https://ipfs.io/');
+    const storage = 'QmfAkb4ksqYi4dmX8sUwaZ1BP6qdcbZ2JWMoW77nx9BAKD';
+
+    async function file(storage) {
+        const chunks = [];
+        for await (const chunk of ipfs.cat(storage)) {chunks.push(chunk);}
+        return Buffer.concat(chunks).toString().toLowerCase().split(',').map(item => 
+            item.substring(2)).map((item, index) => index === 0 ? `0x${item}` : item);
+    }
+      
+    useEffect(() => {file(storage).then(setList);}, []);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        newList.includes(wallet.toLowerCase()) ? 
+            setStatus("YOU ARE WHITELISTED!") : 
+            setStatus("YOU ARE NOT WHITELISTED!");
+    }
 
     return (
 
@@ -8,13 +33,23 @@ export default function Checker() {
 
             <div className="checker_page_text_content">
 
-                <h1>CHECKER PAGE</h1>
-                <p>THIS IS THE CHECKER PAGE</p>
-            
+                <form id="checker_form" onSubmit={handleSubmit}>
+                    
+                    <p>ENTER YOUR WALLET</p>
+                    
+                    <input type="text" value={wallet} 
+                        onChange={(e) => setWallet(e.target.value)}/>
+                    
+                </form>
+
+                <button type="submit" form="checker_form">CHECK</button>
+
+                <p className="checker_page_content">{status}</p>
+
             </div> 
 
         </div>
 
     )
-    
+
 }
