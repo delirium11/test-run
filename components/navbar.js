6e106b8b5e5416f8167d7a0288c60a6ff60a6232
delirium11 <pervane.uk@gmail.com>
2023-04-21 17:49:29 -0400

    commit 61

--- components/navbar.js
+++ components/navbar.js
@@ -12,11 +12,12 @@ export default function Navbar() {
     const [ balance, setBalance ] = useState(null);
     const [ signer, setSigner ] = useState(null);
     const [ status, setStatus ] = useState('CONNECT');
-            
-    useEffect(() => {
-        async function fetchWallet() {
-            if (window.ethereum && window.ethereum.selectedAddress) {
+
+    async function fetchWallet() {
+        if (window.ethereum && window.ethereum.selectedAddress) {
+            try {
                 const provider = new ethers.providers.Web3Provider(window.ethereum);
+                const accounts = await window.ethereum.request({ method: 'eth_accounts' });
                 const signer = provider.getSigner();
                 const address = await signer.getAddress();
                 const balance = await provider.getBalance(address);
@@ -24,42 +25,36 @@ export default function Navbar() {
                 setSigner(signer);
                 setAddress(address);
                 setBalance(balance);
-                setStatus(address.substring(0, 2) + '...' + address.substring(38));
-                console.log('PROVIDER:', provider);
-                console.log('SIGNER:', signer);
-                console.log('ADDRESS:', address);
-                console.log('BALANCE:', balance);
-                (address != null) ? setAddress(address) : null;
-                window.ethereum.on('accountsChanged', (accounts) => {
-                    setAddress(accounts[0]);
-                    (accounts.length === 0) ? setStatus('CONNECT') : (fetchWallet());
+                setStatus(address.substring(38));
+                if (accounts.length > 0) {
+                    setAddress(accounts[0])
+                }
+                window.ethereum.on('accountsChanged', (newAccounts) => {
+                    setAddress(newAccounts[0]);
                 });
+            } catch (error) {
+                console.error(error);
             }
         }
-        fetchWallet();
+    }
+
+    console.log('ADDRESS:', address);
+
+
+    useEffect(() => {
+
+        try {
+            fetchWallet() 
+        } catch (error) {
+            console.error(error);
+        }
+
     }, []);
     
     async function connectWallet() {
         if (window.ethereum) {
             await window.ethereum.request({ method: 'eth_requestAccounts' });
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
-            const signer = provider.getSigner();
-            const address = await signer.getAddress();
-            const balance = await provider.getBalance(address);
-            setProvider(provider);
-            setSigner(signer);
-            setAddress(address);
-            setBalance(balance);
-            setStatus(address.substring(0, 2) + '...' + address.substring(38));
-            console.log('PROVIDER:', provider);
-            console.log('SIGNER:', signer);
-            console.log('ADDRESS:', address);
-            console.log('BALANCE:', balance);
-            (address != null) ? setAddress(address) : null;
-            window.ethereum.on('accountsChanged', (accounts) => {
-                setAddress(accounts[0]);
-                (accounts.length === 0) ? setStatus('CONNECT') : (fetchWallet());
-            });
+            fetchWallet();
         } else {
             alert('METAMASK NOT DETECTED')
         }
