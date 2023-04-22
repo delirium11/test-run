0b1f5831797a561241dced9990d70fb9c50e8339
delirium11 <pervane.uk@gmail.com>
2023-04-21 20:57:52 -0400

    commit 78

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import styles from '../styles/globals.css'
 import Navbar from '@/components/navbar'
+import { useEffect } from 'react'
 
 export default function App ({ Component, pageProps }) {
+    
+    useEffect(() => {
+        const handleBeforeUnload = () => {
+            window.localStorage.setItem("userRefresh", "true");
+        }
+        window.addEventListener("beforeunload", handleBeforeUnload);
+        return () => {
+            window.removeEventListener("beforeunload", handleBeforeUnload);
+        };
+    }, []);
+    
+      useEffect(() => {
+        if (window.localStorage.getItem("userRefresh")) {
+            window.localStorage.removeItem("userRefresh");
+        } else {
+            location.reload(true);
+        }
+    }, []);
 
     return (
 
