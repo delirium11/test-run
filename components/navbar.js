4738147a3ea3fac4cc911e48cddd6660eca193d8
delirium11 <pervane.uk@gmail.com>
2023-11-26 01:05:24 -0500

    commit 137

--- components/navbar.js
+++ components/navbar.js
@@ -29,7 +29,8 @@ export default function Navbar() {
     if (typeof window !== 'undefined') {
         window.addEventListener('scroll', function() {
             if (window.scrollY > 0) {
-                document.querySelector(`.${styles.navbar}`).classList.add(`${styles.scrolled}`);
+                document.querySelector(`.${styles.navbar}`)
+                .classList.add(`${styles.scrolled}`);
             } else {
                 document.querySelector(`.${styles.navbar}`)
                 .classList.remove(`${styles.scrolled}`);
