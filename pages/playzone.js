7fd84da1659ffdb46432e3a743b56225493acf24
delirium11 <pervane.uk@gmail.com>
2023-11-26 20:06:14 -0500

    commit 141

--- pages/playzone.js
+++ pages/playzone.js
@@ -6,25 +6,56 @@ export default function Playzone() {
 
         <div className={styles.RMPageContainer}>
 
-            <svg className={styles.RMButton1}>
+            <h1 className={styles.RMPageTitle}>
 
-                <path stroke="white" stroke-width="3" fill="yellow"
-                
-                    d=" M10,10 L250,10 L390,101 L250,190 L10,190 Z "
-                
-                />
+                THE JOURNEY OF LUCKY CHARMS
 
-            </svg>
-            
-            <svg className={styles.RMButton2}>
+            </h1>
 
-                <path 
-                    
-                    d="  "
-                
-                />
+            <div className={styles.RMSVGContainer}> 
 
-            </svg>
+                <svg className={styles.RMButton1} viewBox="0 0 100 100">
+
+                    <path 
+
+                        fill="yellow" stroke="green" stroke-width="5"
+
+                        d=" 
+                        
+                            M    0,   0
+                            L  100,   0
+                            L  100, 100
+                            L    0, 100
+                            Z
+
+                        "
+
+                    />
+
+                </svg>
+
+                <svg className={styles.RMButton2} viewBox="0 0 400 200">
+
+                    <path 
+
+                        stroke="white" stroke-width="3" fill="yellow"
+                        
+                        d=" 
+                        
+                            M  10, 10 
+                            L 250, 10 
+                            L 390, 101 
+                            L 250, 190 
+                            L  10, 190 
+                            Z 
+                        
+                        "
+
+                    />
+
+                </svg>
+
+            </div>
 
         </div>
 
