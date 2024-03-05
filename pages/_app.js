7f18bd7bff171b81cea00a7bdfe5b107d13ae214
delirium11 <pervane.uk@gmail.com>
2024-03-05 14:59:49 -0500

    commit 177

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,31 @@ export default function App({ Component, pageProps }) {
   const [loading, setLoading] = useState(true);
   const [opacity, setOpacity] = useState(1);
 
-  console.log("This is the API Key: " + process.env.WLMS_FIREBASE_API_KEY)
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+  console.log("this is the api key: " + apiKey);
+
+  useEffect (() => {
+    firebase.initializeApp(firebaseConfig);
+    const db = firebase.firestore();
+    const collectionRef = db.collection('whitelist');
+    const newEntryRef = collectionRef.doc('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
+    newEntryRef.set({
+      walletAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+      highScore: 35743,
+      timestamp: new Date().getTime(),
+      merkleProof: []
+    })
+  }, []);
+
+  useEffect(() => {
+    firebase.initializeApp(firebaseConfig);
+    const db = firebase.firestore();
+    const docRef = db.collection('whitelist').doc('0xABCDEF1234567890ABCDEF1234567890');
+    docRef.get().then((doc) => {
+      console.log('Retreived data:', doc.data());
+    })
+  }, []);
 
   useEffect(() => {
     if (loading) {
